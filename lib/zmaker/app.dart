adda7d5114608c4d8b8327911cf0e6a9f7d56f7e
thetko1996 <thetkomaung966@gmail.com>
2025-11-13 02:16:22 +0900

    feat: Enable Material 3 and set dark theme for the app
    This commit enables Material 3 and sets the theme to dark, providing a more modern and visually appealing user experience.

--- lib/zmaker/app.dart
+++ lib/zmaker/app.dart
@@ -4,12 +4,17 @@ import 'presentation/stats/sleep_chart_screen.dart';
 
 class ZMakerApp extends StatelessWidget {
   const ZMakerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Z Maker',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal, brightness: Brightness.dark),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.teal,
+        brightness: Brightness.dark,
+      ),
       home: const HomeScreen(),
       routes: {'/stats': (_) => const SleepChartScreen()},
     );
